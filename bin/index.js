cee661811cce94e7830a8c6b4684a667bf836d94
Kassandra Perch <kassandra@kassandraperch.com>
2015-10-12 16:19:29 -0500

    Refactored, now fetches voodoospark from GitHub

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,78 @@
 #!/usr/bin/env node
-
-var path = require('path');
 var particle = require('spark');
 var inquirer = require('inquirer');
 var chalk = require('chalk');
+
+var flash = require('../lib/flash');
+var firmware = require('../lib/firmware');
+
+var command = process.argv[2];
+
+command = command || 'flash';
+
+if(command === 'install'){
+  firmware.install(function(err){
+    if(err){
+      console.error(chalk.red('Error installing firmware: ' + err));
+      process.exit(1);
+    } else {
+      console.log(chalk.green('Firmaware voodoospark installed and ready to flash!'));
+    }
+  });
+} else if(command === 'update'){
+   firmware.install(function(err){
+    if(err){
+      console.error(chalk.red('Error updating firmware: ' + err));
+      process.exit(1);
+    } else {
+      console.log(chalk.green('Firmaware voodoospark updated and ready to flash!'));
+    }
+  }); 
+} else if(command === 'flash'){
+  firmware.checkFirmware(function(err){
+    if(err){
+      console.log(chalk.yellow('Firmware not found-- we need to install it!'));
+      firmware.install(function(err){
+        if(err){
+          console.error(chalk.red('Problem fetching firmware: ' + err));
+          process.exit(1);
+        }
+        console.log(chalk.green('Firmware installation complete!'));
+        startFlashing();
+      });
+    } else {
+      console.log(chalk.green('firmware found.'))
+      startFlashing();
+    }
+  });
+
+  function startFlashing(){
+    inquirer.prompt(loginQuestions, function(answers){
+      flash.getDevices(answers.username, answers.password,
+      function(err, devices){
+        inquirer.prompt([{
+          type: "list",
+          name: "device",
+          message: "Select the device you want to flash",
+          choices: devices
+        }], function(answers){
+          var device = devices.filter(function(device){
+            return device.attributes.name === answers.device
+          })[0];
+          flash.flash(device, function(err){
+            if(err){
+              console.error(chalk.red(err));
+              process.exit(1);
+            } else {
+              console.log(chalk.green("SUCCESS! You should be able to use Johnny-Five now."));
+              process.exit(0);
+            }
+          });
+        });
+      });
+    });
+  }
+}
 
 var loginQuestions = [
   {
@@ -18,47 +87,40 @@ var loginQuestions = [
   }
 ];
 
-inquirer.prompt(loginQuestions, function(answers){
-  particle.login({
-    username: answers.username,
-    password: answers.password
-  }, function(err, body){
-    if(err){
-      if(err.toString().indexOf('invalid_grant') !== -1){
-        console.error(chalk.yellow('Invalid username or password!'));
-      } else {
-        console.error(err);
-      }
-      process.exit(1);
-    }
+// inquirer.prompt(loginQuestions, function(answers){
+//   particle.login({
+//     username: answers.username,
+//     password: answers.password
+//   }, function(err, body){
+//     if(err){
+//       if(err.toString().indexOf('invalid_grant') !== -1){
+//         console.error(chalk.yellow('Invalid username or password!'));
+//       } else {
+//         console.error(err);
+//       }
+//       process.exit(1);
+//     }
 
-    console.log(chalk.green('Successfully logged in to Particle!'));
+//     console.log(chalk.green('Successfully logged in to Particle!'));
 
-    particle.listDevices(function(err, devices){
-      if(err){
-        console.error(err);
-        process.exit(1);
-      }
+//     particle.listDevices(function(err, devices){
+//       if(err){
+//         console.error(err);
+//         process.exit(1);
+//       }
 
-      inquirer.prompt([{
-        type: "list",
-        name: "device",
-        message: "Select the device you want to flash",
-        choices: devices
-      }], function(answers){
-        var device = devices.filter(function(device){
-          return device.attributes.name === answers.device
-        })[0];
+//       inquirer.prompt([{
+//         type: "list",
+//         name: "device",
+//         message: "Select the device you want to flash",
+//         choices: devices
+//       }], function(answers){
+//         var device = devices.filter(function(device){
+//           return device.attributes.name === answers.device
+//         })[0];
 
-        device.flash([path.resolve(__dirname, '..', 'firmware', 'voodoospark.cpp')], function(err){
-          if(err){
-            console.log(err);
-            process.exit(1);
-          } else {
-            console.log(chalk.green("SUCCESS! You should be able to use Johnny-Five now."));
-          }
-        });
-      });
-    });
-  });
-});
+
+//       });
+//     });
+//   });
+// });
